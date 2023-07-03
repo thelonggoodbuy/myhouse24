a0a31d25a1cfc5d573ecc47d31261371b059cd55
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2023-07-03 14:52:32 +0300

    I have changed copy type

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -17,7 +17,7 @@ then
 fi
 
 # python manage.py flush --no-input
-# python manage.py makemigrations
-# python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 exec "$@"
